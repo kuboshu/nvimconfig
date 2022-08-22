42e65fdc5550411ee5ca835cd789585245d539de
kuboshu83 <kuboshu83@gmail.com>
2022-08-23 06:44:56 +0900

    [update]キーマップ更新

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,15 @@
-vim.api.nvim_set_keymap('i', '<C-k>', '<ESC>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-h>', '<BS><DEL>', {noremap = true})
+-- ESC
+vim.api.nvim_set_keymap('i', 'jj', '<ESC>', {noremap = true})
+
+-- バッファ関連
 vim.api.nvim_set_keymap('n', '<C-k>', ':bprev<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-j>', ':bnext<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-@>', ':bd<CR>', {noremap = true})
+
+-- カーソル移動関連
+vim.api.nvim_set_keymap('n', '<C-h>', '<BS><DEL>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-:>', '$', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-a>', '0', {noremap = true})
+
+-- Defx起動 
+vim.api.nvim_set_keymap('n', '<C-;>', ':Defx<CR>', {noremap = true})
